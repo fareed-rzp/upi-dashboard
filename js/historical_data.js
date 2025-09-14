3a0fc82d193a649e78895c6d3c9e85a60aaffe18
fareed-rzp <fareed-rzp@users.noreply.github.com>
2025-09-14 06:28:15 +0000

    deploy: 0220ea5f6d74525f4e111988cb3408707b81dd8d

--- js/historical_data.js
+++ js/historical_data.js
@@ -1,5 +1,5 @@
 // Auto-generated historical UPI data
-// Last updated: 2025-09-13T06:27:34.233374
+// Last updated: 2025-09-14T06:28:14.812025
 
 const historicalUPIData = {
   "Aug-25": {
@@ -189,8 +189,8 @@ const historicalUPIData = {
     "p2pValue": 1761675.49,
     "p2mVolume": 12705.16,
     "p2mValue": 723797.42,
-    "lastUpdated": "2025-09-13T06:27:33.953487",
-    "scrapedAt": "2025-09-13 06:27:33"
+    "lastUpdated": "2025-09-14T06:28:14.534094",
+    "scrapedAt": "2025-09-14 06:28:14"
   }
 };
 
